22862701cce2bb0ecafd2ff2ec6488ce010f2822
2heeesss <hohooodo@gmail.com>
2022-02-22 20:03:29 +0900

    [Feat] add period

--- src/router/api.js
+++ src/router/api.js
@@ -1,14 +1,32 @@
-import express from "express";
-import axios from "axios";
-// import finnhub from "finnhub";
-const finnhub = require("finnhub");
-const yahooFinance = require("yahoo-finance");
+import express from 'express';
+import axios from 'axios';
 
-const api_key = finnhub.ApiClient.instance.authentications["api_key"];
-api_key.apiKey = "c83630aad3ift3bm2d9g";
+// import finnhub from "finnhub";
+const finnhub = require('finnhub');
+const yahooFinance = require('yahoo-finance');
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = 'c83630aad3ift3bm2d9g';
 const finnhubClient = new finnhub.DefaultApi();
 
-const fn = (data) => {
+// Data
+const stockSymbols = ['AAPL', 'TSLA', 'NVDA', 'AMD', 'MSFT', 'AMZN', 'FB'];
+const indexSymbols = ['^AXJO', 'XIN9.FGI', '^STOXX50E', '^FCHI', '^GDAXI', '^HSI', '^N225', '^NSEI', '^AEX', '^STI', '^FTSE', '^NDX', '^RUT', '^GSPC', '^DJI', '^VIX'];
+const cryptoSymbols = [
+  'BINANCE:BTCUSDT',
+  'BINANCE:BCNBTC',
+  'BINANCE:FARMBUSD',
+  'BINANCE:XRPETH',
+  'BINANCE:AGIXBTC',
+  'BINANCE:RENBTCBTC',
+  'BINANCE:DENTETH',
+  'BINANCE:EZBTC',
+  'BINANCE:ARPABTC',
+  'BINANCE:AXSETH',
+  'BINANCE:MOVRUSDT',
+];
+
+const fn = data => {
   const LEN = data.h.length;
   const arr = Array.from({ length: LEN }, () => {});
   const origin = Object.values(data);
@@ -26,20 +44,24 @@ const fn = (data) => {
   };
 };
 
-// 15, 30, 60, 1D, 1M 다 따로?
-// to ~ from 어떤식으로 결정하지
-const getStockCandles = async () => {
+const periodToSeconds = {
+  15: 60 * 15 * 100 * 4,
+  30: 60 * 30 * 100 * 2,
+  60: 60 * 60 * 100,
+  D: 60 * 60 * 24 * 80,
+  W: 60 * 60 * 24 * 7 * 80,
+  M: 60 * 60 * 24 * 30 * 80,
+};
+
+const getStockCandles = async period => {
+  const start = Math.floor(Date.now() / 1000) - periodToSeconds[period];
+  const end = Math.floor(Date.now() / 1000);
+
   try {
-    return await new Promise((resolve) => {
-      finnhubClient.stockCandles(
-        "AAPL",
-        "60",
-        1643691600, // 미국시간 : 2월 1일 0시
-        1644379200, // 미국시간 : 2월 8일 23시
-        (error, data, response) => {
-          resolve(fn(data));
-        }
-      );
+    return await new Promise(resolve => {
+      finnhubClient.stockCandles('AAPL', period, start, end, (error, data, response) => {
+        resolve(fn(data));
+      });
     });
   } catch (err) {
     console.err(err);
@@ -51,23 +73,17 @@ const getStockCandles = async () => {
 // 우리나라 시간과 미국 시간 변환?
 function getLineData(hours = 1) {
   try {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       const seconds = hours * 60 * 60; // hours * 1시간(3600초)
 
-      finnhubClient.cryptoCandles(
-        "BINANCE:BTCUSDT",
-        "1",
-        Math.floor(Date.now() / 1000) - 3600,
-        Math.floor(Date.now() / 1000),
-        (error, data, response) => {
-          resolve(
-            [...data.c].map((close, idx) => ({
-              close,
-              time: data.t[idx],
-            }))
-          );
-        }
-      );
+      finnhubClient.cryptoCandles('BINANCE:BTCUSDT', '1', Math.floor(Date.now() / 1000) - 3600, Math.floor(Date.now() / 1000), (error, data, response) => {
+        resolve(
+          [...data.c].map((close, idx) => ({
+            close,
+            time: data.t[idx],
+          }))
+        );
+      });
     });
   } catch (err) {
     console.error(err);
@@ -76,18 +92,12 @@ function getLineData(hours = 1) {
 
 function getCandleData(hours = 1) {
   try {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       const seconds = hours * 60 * 60; // hours * 1시간(3600초)
 
-      finnhubClient.cryptoCandles(
-        "BINANCE:BTCUSDT",
-        "1",
-        Math.floor(Date.now() / 1000) - 3600,
-        Math.floor(Date.now() / 1000),
-        (error, data, response) => {
-          resolve(fn(data));
-        }
-      );
+      finnhubClient.cryptoCandles('BINANCE:BTCUSDT', '1', Math.floor(Date.now() / 1000) - 3600, Math.floor(Date.now() / 1000), (error, data, response) => {
+        resolve(fn(data));
+      });
     });
   } catch (err) {
     console.error(err);
@@ -96,11 +106,11 @@ function getCandleData(hours = 1) {
 
 function getMarketInfo() {
   try {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       yahooFinance.quote(
         {
-          symbol: "AAPL",
-          modules: ["price", "summaryDetail"], // see the docs for the full list
+          symbol: 'AAPL',
+          modules: ['price', 'summaryDetail'], // see the docs for the full list
         },
         function (err, quotes) {
           resolve(quotes);
@@ -112,28 +122,65 @@ function getMarketInfo() {
   }
 }
 
+function getQuote(symbol) {
+  try {
+    return new Promise(resolve => {
+      finnhubClient.quote(symbol, (error, data) => {
+        resolve(data);
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// function getCryptoQuote() {
+//   try {
+//     return new Promise((resolve) => {});
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// function getIndexQuote() {
+//   try {
+//     return new Promise((resolve) => {});
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
 const apiRouter = express.Router();
 
-apiRouter.get("/", (req, res) => res.send("Hello"));
+apiRouter.get('/', (req, res) => res.send('Hello'));
 
-apiRouter.get("/crypto/line", async (req, res) => {
+apiRouter.get('/crypto/line', async (req, res) => {
   const data = await getLineData();
   res.status(200).json(data);
 });
 
-apiRouter.get("/crypto/candle", async (req, res) => {
+apiRouter.get('/crypto/candle', async (req, res) => {
   const data = await getCandleData();
   res.status(200).json(data);
 });
 
-apiRouter.get("/stock/candle", async (req, res) => {
-  const data = await getStockCandles();
+apiRouter.get('/stock/candle', async (req, res) => {
+  const data = await getStockCandles(req.query['period']);
+  res.status(200).json(data);
+});
+
+apiRouter.get('/market/info', async (req, res) => {
+  const data = await getMarketInfo();
 
   res.status(200).json(data);
 });
 
-apiRouter.get("/market/info", async (req, res) => {
-  const data = await getMarketInfo();
+apiRouter.get('/quote', async (req, res) => {
+  const {
+    query: { symbol },
+  } = req;
+
+  const data = await getQuote(symbol);
 
   res.status(200).json(data);
 });
