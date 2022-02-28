e8b3a37cb538f26341caf49ffc699745d011569c
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-28 20:05:03 +0900

    [Fix] Add error handling for category query param with market/info API

---
+++ src/router/mockData.js
@@ -0,0 +1,335 @@
+export const NEWS_MOCK_DATA = [
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "NH투자증권, '해외주식 권리정보 조회 서비스' 오픈",
+    url: 'https://www.hankyung.com/finance/article/2022022590266',
+    image: {
+      width: 620,
+      height: 453,
+      contentURL: 'https://img.hankyung.com/photo/202202/01.29067340.1.jpg',
+    },
+    description:
+      "NH투자증권(11,700 +1.74%)은 해외주식 거래고객의 편의성을 높이기 위해 글로벌 IB수준의 '해외주식 권리정보 조회 서비스'를 오픈한다고 25일 밝혔다. 국내에서 제공되고 있는 해외주식 권리정보는 국가마다 절차가 다르고 병합·분할 등의 예정날짜가 빈번히 변동하는 경우가 많아서 고객들의 불편함이 크다. 해외주식 권리확정은 한국예탁원을 거치게 되어있어",
+    provider: '한경닷컴',
+    datePublished: '2022-02-25 02:14:00',
+  },
+  {
+    name: '미국 증권당국, 머스크 형제 테슬라 주식 내부자거래 혐의 조사',
+    url: 'https://www.hankyung.com/international/article/202202258945Y',
+    image: {
+      width: 620,
+      height: 482,
+      contentURL: 'https://img.hankyung.com/photo/202202/ZK.29067112.1.jpg',
+    },
+    description:
+      '일론 머스크 동생, 작년 11월 형 "주식 팔까?" 트윗 직전 주식 매각 머스크 "동생은 트위터 설문 몰랐다" 미국 증권거래위원회(SEC)가 테슬라 최고경영자(CEO) 일론 머스크와 그의 동생 킴벌 머스크의 테슬라 주식 매각이 내부자 거래 규정을 위반했는지를 조사하고 있다고 월스트리트저널(WSJ)이 소식통을 인용해 24일(현지시간) 보도했다. 킴벌은 지난해',
+    provider: '한경닷컴',
+    datePublished: '2022-02-25 01:56:00',
+  },
+  {
+    name: "삼성증권, '해외주식 다모아' 이벤트",
+    url: 'https://www.edaily.co.kr/news/read?newsId=01748246632234128',
+    image: {
+      width: 246,
+      height: 700,
+      contentURL:
+        'https://image.edaily.co.kr/images/Photo/files/NP/S/2022/02/PS22022500258.jpg',
+    },
+    description:
+      '삼성증권(016360)은 ‘해외주식 다모아’ 이벤트를 오는 28일까지 진행한다고 25일 밝혔다.이벤트 참여방법은 먼저 삼성증권 홈페이지 혹은 모바일 앱 엠팝(mPOP)에서 해당 이벤트를 신청한 후 타사에사 보유 중인 해외주식(해외 시장에 상장된 해외주식, ETF',
+    provider: 'edaily.co.kr',
+    datePublished: '2022-02-25 00:53:00',
+  },
+  {
+    name: '디캠프, 2월 디데이서 와인 투자 플랫폼 ‘블링커스 주식회사’ 우승',
+    url: 'https://www.msn.com/ko-kr/news/other/%EB%94%94%EC%BA%A0%ED%94%84-2%EC%9B%94-%EB%94%94%EB%8D%B0%EC%9D%B4%EC%84%9C-%EC%99%80%EC%9D%B8-%ED%88%AC%EC%9E%90-%ED%94%8C%EB%9E%AB%ED%8F%BC-%EB%B8%94%EB%A7%81%EC%BB%A4%EC%8A%A4-%EC%A3%BC%EC%8B%9D%ED%9A%8C%EC%82%AC-%EC%9A%B0%EC%8A%B9/ar-AAUh3Gf',
+    image: {
+      width: 569,
+      height: 314,
+      contentURL:
+        'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAUgQqP.img?h=315&w=600&m=6&q=60&o=t&l=f&f=jpg',
+    },
+    description:
+      'DGB금융그룹, NH농협은행, 하나벤처스, 한국핀테크지원센터와 공동 주최한 창업 경쟁 플랫폼 디데이에서 블링커스 주식회사가 우승했다고 25일 밝혔다. 2월 디데이는 은행권청년창업재단의 출연 은행과 협업해 기획됐다. NFT, 블록체인 등 금융서비스에 응용 가능한 혁신 기업 발굴에 중점을 뒀다. 본선에 오른 5개 팀은 14대 1의 경쟁률을 뚫고 최종 본선에 올',
+    provider: 'MSN',
+    datePublished: '2022-02-25 01:19:01',
+  },
+  {
+    name: '주식회사 담심포, 비대면 봉사활동 키트 ‘점자촉각 놀이교구재’ 출시',
+    url: 'http://www.sisa-news.com/news/article.html?no=190611',
+    image: {
+      width: 650,
+      height: 269,
+      contentURL:
+        'http://www.sisa-news.com/data/photos/20220208/art_164575612701_1b9974.jpg',
+    },
+    description:
+      '[시사뉴스 김남규 기자] 사회적기업 주식회사 담심포(대표 박귀선)는 새마을금고중앙회(회장 박차훈)에서 주최하는 ‘MG희망나눔 소셜 성장 지원사업’을 통해 시각장애 아동의 점자 학습과 사회성 향상을 위한 ‘점자촉각놀이교구재’를 출시했다고 24일 밝혔다. ‘점자촉각놀이교구재’는 비대면 봉사활동 kit 상품으로 개발되었다. 맹학교 선생님과 시각장애아동 부모와 함',
+    provider: 'sisa-news',
+    datePublished: '2022-02-25 02:29:00',
+  },
+  {
+    name: '윌라, 박영옥 ‘주식투자 절대 원칙’ 오디오북 공개',
+    url: 'https://www.msn.com/ko-kr/news/other/ec-9c-8c-eb-9d-bc-eb-b0-95-ec-98-81-ec-98-a5-e2-80-98-ec-a3-bc-ec-8b-9d-ed-88-ac-ec-9e-90-ec-a0-88-eb-8c-80-ec-9b-90-ec-b9-99-e2-80-99-ec-98-a4-eb-94-94-ec-98-a4-eb-b6-81-ea-b3-b5-ea-b0-9c/ar-AAUgI8v',
+    image: {
+      width: 570,
+      height: 304,
+      contentURL:
+        'https://image.fnnews.com/resource/media/image/2022/02/25/202202250837024334_l.jpg',
+    },
+    description:
+      '[파이낸셜뉴스] 윌라가 ‘주식농부’ 박영옥 대표 저술의 ‘주식투자 절대 원칙’을 오디오북으로 공개했다.‘주식투자 절대 원칙’은 박영옥 대표가 자본금 4500만원으로 2000억원의 부를 이룬 투자 비결을 담았다. 미디어를 통해 개미 투자자들에게 익히 알려진 존 리, 이채원, 김동환 등 투자 전문가들도 추천의 ..',
+    provider: '파이낸셜뉴스 on MSN.com',
+    datePublished: '2022-02-24 23:37:22',
+  },
+  {
+    name: `"머스크 형제, 테슬라 주식 매각 건으로 SEC '내부자거래' 조사 받아"`,
+    url: 'https://www.asiae.co.kr/article/2022022508191545341',
+    image: {
+      width: 700,
+      height: 323,
+      contentURL:
+        'https://cphoto.asiae.co.kr/listimglink/1/2022022508164658144_1645744606.png',
+    },
+    description:
+      '일론 머스크 테슬라 최고경영자(CEO)와 그의 남동생인 킴벌 머스크가 지난해 갑작스럽게 테슬라 지분을 매도한 것과 관련해 미국 증권거래위원회(SEC)의 조사를 받고 있는 것으로 확인됐다고 24일(현지시간) 월스트리트저널(WSJ)이 보도했다. WSJ는 익명의 소식통을 인용해 SEC가 머스크 형제의 내부자거래 혐의를 살펴보고 있다고 전했다. 지난해 11월 머스',
+    provider: 'asiae.co.kr',
+    datePublished: '2022-02-24 23:19:00',
+  },
+  {
+    name: '우크라이나 침공에 러시아 관련 투자자산 출렁···주식·채권 ...',
+    url: 'http://www.sisajournal-e.com/news/articleView.html?idxno=256058',
+    image: {
+      width: 600,
+      height: 255,
+      contentURL:
+        'http://www.sisajournal-e.com/news/photo/202202/256058_107070_5739.jpg',
+    },
+    description:
+      '[시사저널e=송준영 기자] 러시아가 우크라이나와의 전쟁을 본격화하면서 관련 투자자산들이 요동치고 있다. 러시아 주식 시장은 장중 50% ...',
+    provider: 'sisajournal-e',
+    datePublished: '2022-02-25 01:24:00',
+  },
+  {
+    name: '삼성증권 "해외주식 입고·매매 시 최대 500만원 리워드"',
+    url: 'https://biz.newdaily.co.kr/site/data/html/2022/02/25/2022022500038.html',
+    image: {
+      width: 382,
+      height: 472,
+      contentURL:
+        'https://image.newdaily.co.kr/site/data/img/2022/02/25/2022022500038_0.png',
+    },
+    description:
+      '삼성증권은 타사 보유 해외 주식 입고 이벤트를 이달 28일까지 진행한다고 25일 밝혔다.타사에 보유하고 있는 해외주식을 1000만원 이상 삼성증권에 입고하고, 해당 금액 이상 해외주식을 오는 28일까지 거래, 5월 31일까지 잔고를 유지하면 입고 금액에 따라 리워드 혜택을 제공한다.입고 금액과 거래금액은 이벤트 기간 중 누적으로 합산되며, 1000만원 이상',
+    provider: '뉴데일리 경제',
+    datePublished: '2022-02-25 01:48:00',
+  },
+  {
+    name: '"이제 주식 염증난다" 10% 금리에 청년 200만 몰린 이유',
+    url: 'https://www.msn.com/ko-kr/news/national/%EC%9D%B4%EC%A0%9C-%EC%A3%BC%EC%8B%9D-%EC%97%BC%EC%A6%9D%EB%82%9C%EB%8B%A4-10percent-%EA%B8%88%EB%A6%AC%EC%97%90-%EC%B2%AD%EB%85%84-200%EB%A7%8C-%EB%AA%B0%EB%A6%B0-%EC%9D%B4%EC%9C%A0/ar-AAUgwPJ',
+    image: {
+      width: 559,
+      height: 314,
+      contentURL:
+        'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAUgslZ.img?h=315&w=600&m=6&q=60&o=t&l=f&f=jpg',
+    },
+    description:
+      '금융업계에서는 주식·암호화폐 시장이 하락세로 돌변하면서 ‘불확실성’의 시대를 체감한 2030 세대들이 청년희망적금으로 몰린 것으로 분석했다. 1년에 10%의 이자에 청년들이 몰리는 현상에 대해 적지 않은 청년들은 “그게 어디냐”라는 반응을 보였다.',
+    provider: 'MSN',
+    datePublished: '2022-02-24 21:55:00',
+  },
+  {
+    name: "우크라 전쟁 피해·주식 손실까지…시민들 반중이어 '반러 정서'",
+    url: 'https://www.msn.com/ko-kr/news/national/ec-9a-b0-ed-81-ac-eb-9d-bc-ec-a0-84-ec-9f-81-ed-94-bc-ed-95-b4-c2-b7-ec-a3-bc-ec-8b-9d-ec-86-90-ec-8b-a4-ea-b9-8c-ec-a7-80-e2-80-a6-ec-8b-9c-eb-af-bc-eb-93-a4-eb-b0-98-ec-a4-91-ec-9d-b4-ec-96-b4-eb-b0-98-eb-9f-ac-ec-a0-95-ec-84-9c/ar-AAUh49n',
+    image: {
+      width: 700,
+      height: 465,
+      contentURL:
+        'http://image.newsis.com/2022/02/25/NISI20220225_0018527152_web.jpg',
+    },
+    description:
+      '[서울=뉴시스]신재현 최영서 기자 = 러시아 군이 우크라이나 침공을 본격 개시하며 전쟁에 돌입하자 국내 일각에서는 반러 정서가 고조되고 있다. 러시아가 명분 없이 전쟁을 시작했다는 비판과 주식장 폭락으로 피해를 봤다는 목소리 등이 잇따라 나온다. 25일 외신 등에 따르면 러시아군은 블라디미르 푸틴 대통령의 명령을 받고 전날 우크라이나 동부, 북부, 남부 등',
+    provider: 'newsis.com on MSN.com',
+    datePublished: '2022-02-25 01:55:59',
+  },
+  {
+    name: '[표] 국내투자자 중국 주식투자 상위 종목(24일)',
+    url: 'https://news.einfomax.co.kr/news/articleView.html?idxno=4200731',
+    image: {
+      width: 100,
+      height: 100,
+      contentURL: 'https://ppss.kr/wp-content/uploads/2020/07/01-4-540x304.png',
+    },
+    description:
+      '※ 다음은 24일 국내투자자의 중국 주식투자 상위 종목이다. (단위: USD. 순매수결제 기준) 출처: 한국예탁결제원 SEIBro (서울=연합인포맥스)(끝)본 기사는 인포맥스 금융정보 단말기에서 2시간 더 빠른 08시 18분에 서비스된 기사입니다.',
+    provider: 'einfomax.co.kr',
+    datePublished: '2022-02-25 01:19:00',
+  },
+  {
+    name: '③ “서학개미 덕분”...해외주식 수수료로 BK 수익 방어',
+    url: 'https://www.joseilbo.com/news/htmls/2022/02/20220225447414.html',
+    image: {
+      width: 500,
+      height: 495,
+      contentURL:
+        'http://www.joseilbo.com/gisa_img/16457447401645744740_jskim.JPG',
+    },
+    description:
+      '지난해 증권사들이 주식을 중개해 받은 수수료 수익은 8조원에 육박했다. 다만 증시 거래대금이 1월을 고점으로 지속 감소하면서 위탁매매(BK, 브로커리지) 부문 수익도 매분기 줄어드는 상황이다. 이 가운데 해외주식 중개로 벌어들인 외화증권수탁 수수료 수익은 전년보다 55.7% 급증해 눈길을 끈다. 25일 금융투자협회 전자공시에 따르면 12월 결산법인 52개',
+    provider: 'joseilbo.com',
+    datePublished: '2022-02-24 23:20:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+  {
+    name: "윌라, '주식농부' 박영옥 '주식투자 절대 원칙' 오디오북 공개",
+    url: 'https://www.insight.co.kr/news/383860',
+    image: {
+      width: 700,
+      height: 371,
+      contentURL:
+        'https://img.insight.co.kr/static/2022/02/25/1200/img_20220225111242_v95833ok.jpg',
+    },
+    description:
+      "[인사이트] 김소영 기자 = 윌라가 '주식농부' 박영옥 대표 저술의 '주식투자 절대 원칙'을 오디오북으로 공개했다고 밝혔다. '주식투자 절대 원칙'은 박영옥 대표가 자본금 4,500만원으로 2,000억 부를 이룬 투자 비결을 담은 책이다. 미디어를 통해 개미 ...",
+    provider: 'insight.co.kr',
+    datePublished: '2022-02-25 02:40:00',
+  },
+];
